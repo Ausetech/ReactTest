e4a363f82aa6e7f3562d836d50475db615a772c3
Srimali <jasrimalimadurangani@gmail.com>
2025-05-20 13:31:58 +0530

    Make some changes

--- src/Pages/UsersPage.js
+++ src/Pages/UsersPage.js
@@ -4,21 +4,30 @@ import React, {use, useEffect, useState} from 'react';
 const UsersPage = () => {
     const [users, setUsers] = useState([]);
 
-    useEffect (()) =>
-    fetch ('https://jsonplaceholder.typicode.com/users')
+    useEffect (() => { fetch ('https://jsonplaceholder.typicode.com/users')
 
     .then (res => res.json())
     .then (data => setUsers(data))
-
     .catch (err => console.error("Error while fetching data", err));
-} ); 
+   
+} , []); 
 
-return (
-    <div> 
-        <UserCard = {user.id} user = {user} />      
+    return (
+        <div>
+            <h1>Users</h1>
+            {users.map(user => (
+                <UserCard key={user.id} user={user} />
+            ))}
         </div>
+    );
+};
+
+// return (
+//     <div> 
+//         <UserCard = {user.id} user = {user} />      
+//         </div>
     
-);
+// );
 
 export default UsersPage;
 
